da1982b54aadc60b9034998991d4fc2f1d3b69bf
Remko Duursma <remkoduursma@gmail.com>
2017-12-19 13:51:41 +1100

    first commit

---
+++ R/fuelpricensw-package.r
@@ -0,0 +1,6 @@
+#' Fuel prices in New South Wales
+#'
+#' Near-daily fuel prices for nearly all service stations in New South Wales, between August 2016 and October 201
+#' @name fuelpricensw
+#' @docType package
+NULL
